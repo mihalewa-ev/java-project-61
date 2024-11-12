b96aa82f7147d83bf0772d6dab9395a485029bae
elena <mihalewa1995@gmail.com>
2024-11-12 23:31:13 +0500

    Добавлено игровое приветствие

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,3 +1,7 @@
-public static void main(String[] args) {
-    System.out.println("Hello World!");
+package hexlet.code;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Brain Games!");
+    }
 }
